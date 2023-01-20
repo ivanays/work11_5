f7bbbc3870ca9b775ff8dfb995cb673bd25e6c42
ivanays <ivanays@yandex.ru>
2023-01-20 20:06:49 +0300

    work11_5 v1.01

---
+++ DB.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('Social_network', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = { Sequelize };
